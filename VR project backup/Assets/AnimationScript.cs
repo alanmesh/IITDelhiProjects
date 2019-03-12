078e22a0eec95d1ae9c7c96c7447aeb2b52bc560
Alankar Meshram <alankar.0014@gmail.com>
2019-03-12 19:19:39 +0530

    Add files via upload

---
+++ VR project backup/Assets/AnimationScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour {
+
+    // Use this for initialization
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        anim.SetBool("running", true);
+        anim.ResetTrigger("TurnLeft");
+        anim.ResetTrigger("TurnRight");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    public void TurnLeft()
+    {
+        anim.SetBool("running", false);
+        anim.SetTrigger("TurnLeft");
+        anim.SetBool("running", true);
+    }
+    public void TurnRight()
+    {
+        anim.SetBool("running", false);
+        anim.SetTrigger("TurnRight");
+        anim.SetBool("running", true);
+    }
+}
